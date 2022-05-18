ef3e35aae25a60fecf8206f3d8cd7d11512647fd
Nia Ardiati <ardiatinia01@gmail.com>
2022-05-18 22:22:06 +0700

    Implementasi Thread dan Runnable (Simple Thread)

---
+++ app/src/main/java/com/example/threadexample4/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.threadexample4
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.activityUiThread
+import org.jetbrains.anko.doAsync
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        button.setOnClickListener {
+       doAsync {
+           for (i in 1..15){
+               Thread.sleep(2000)
+               activityUiThread {
+                   textView.text = i.toString()
+                }
+             }
+          }
+        }
+    }
+}
